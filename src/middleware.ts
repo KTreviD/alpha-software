58b6596133f4f22d7b17fae911c7663d10304d16
KTreviD <carloscharlie4td@hotmail.com>
2026-02-03 02:26:09 -0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,8 +26,10 @@ export default function middleware(req: NextRequest) {
 
   const accessToken = req.cookies.get("accessToken")?.value;
 
+  const isProtectedRoute = protectedRoutes.some(route =>
+    path.startsWith(route)
+  );
   const isPublicRoute = publicRoutes.includes(path);
-  const isPublicRoute = publicRoutes.some(route => path.startsWith(route));
 
   if (isProtectedRoute && !accessToken) {
     return NextResponse.redirect(new URL("/", req.nextUrl));
